3eb9a81f2e6236063d3114a8b320739ae736ded5
lacho55 <lacho_fitness@abv.bg>
2020-06-18 17:12:28 +0300

    ex08

--- extraEx2/ex08.hs
+++ extraEx2/ex08.hs
@@ -1,3 +1,25 @@
 main :: IO()
 main = do 
-    
+    print(highestCapital count1)
+
+
+type Name = String
+type Capital = Name
+type AvgYearlyTemperature = Double
+type Elevation = Int
+
+data City = City Name Elevation AvgYearlyTemperature deriving(Read, Show)
+data Country = Country Name Capital [City] deriving(Read, Show)
+
+count1 :: [Country]
+count1 = [(Country "France" "Paris" [(City "Verdun" 2 4), (City "Paris" 2 2)]),
+          (Country "Germany" "Berlin" [(City "Baden Baden" 1 1), (City "Berlin" 1 1)])]
+
+highestCapital :: [Country] -> Name
+highestCapital allCountries = capWithMaxHight
+    where
+        listOfCapsAndEls =  [(capName, citEl) | (Country _ capName listCities) <- allCountries, (City citName citEl _) <- listCities, capName == citName]
+        maxHight = maximum [ x | (_,x) <- listOfCapsAndEls]
+        capWithMaxHight = head [ x | (x, y) <- listOfCapsAndEls, y == maxHight]
+
+
